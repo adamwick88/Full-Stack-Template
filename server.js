4ad4e8984a3ee2463c73df4571a34de8eed2087a
Adam Wickstrom <adamwick1988@gmail.com>
2022-06-23 20:23:05 -0500

    updated apps

--- server.js
+++ server.js
@@ -2,16 +2,26 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const MongoClient=require('mongodb').MongoClient
+
 require('dotenv').config()
- 
+const PORT=8000
 let db,
 dbConnectionString=process.env.DB_STRING
-dbName = 'star-wars-quotes',
-collection
+dbName = 'Star_Wars_Fun'
+let collection
 
 MongoClient.connect(dbConnectionString)
 .then(client =>{
     console.log('Connected to Databae')
     db=client.db(dbName)
-    collection=db.collection('quotes')
+    collection=db.collection('quote')
+})
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running on port = ${PORT}`)
 })
