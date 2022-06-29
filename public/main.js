028da1a3612949bb2bf95a4dc392c197d932af86
Adam Wickstrom <adamwick1988@gmail.com>
2022-06-28 19:40:54 -0500

    updates

---
+++ public/main.js
@@ -0,0 +1,2 @@
+//This is where you put the code that reaches out to server and sends information back and forth
+//Info can travel back and forth using fetches
--- server.js
+++ server.js
@@ -1,27 +1,38 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const MongoClient=require('mongodb').MongoClient
-
+const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
-const PORT=8000
+const PORT = 8000
+
 let db,
-dbConnectionString=process.env.DB_STRING
-dbName = 'Star_Wars_Fun'
-let collection
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'sample_mflix',
+    collection
 
 MongoClient.connect(dbConnectionString)
-.then(client =>{
-    console.log('Connected to Databae')
-    db=client.db(dbName)
-    collection=db.collection('quote')
-})
+    .then(client => {
+        console.log(`Connected to Database`)
+        db = client.db(dbName)
+        collection = db.collection('movies')
+    })
 
 app.set('view engine', 'ejs')
-
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
 
+app.get('/', async (request, response) => {
+    try {
+        response.render('index.ejs')
+    } catch (error) {
+        response.status(500).send({message: error.message})
+    }
+})
+
+
+//PORT = 8000
 app.listen(process.env.PORT || PORT, () => {
-    console.log(`Server is running on port = ${PORT}`)
+    console.log(`Server is running on port`)
 })
